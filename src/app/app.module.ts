00cf64f1e150fc784dc6f2924fa85c9e809e0e40
Vanessa <vanne.jgs@gmail.com>
2019-10-14 17:37:11 -0500

    primer borrador noticias json rss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './http.service';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,10 +14,11 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,  HttpClientModule,IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
+    HttpService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/noticias/noticias.page.ts
+++ src/app/noticias/noticias.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-noticias',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticiasPage implements OnInit {
 
-  constructor() { }
+  constructor(public http:HttpService) {
+   // this.traerNoticias();
+   }
+  title:string;
+  pubDate:string;
+  link:string;
+  description:string;
+  content:string;
 
   ngOnInit() {
+    
   }
+  noticias:any;
+  
+  // traerNoticias(){
+    
+  //   this.http.traerNoticias().then(
+  //     (inv) => { 
+  //      console.log(inv);   
+  //      this.noticias = inv;      
+  //     },
+  //     (error) =>{
+  //       console.log("Error"+JSON.stringify(error));
+  //       alert("Verifica que cuentes con internet");
+  //     }
+  //   );
+
+  // }
+
+
 
 }
