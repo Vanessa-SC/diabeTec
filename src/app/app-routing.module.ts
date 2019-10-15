62204f865081a3eac3d1b3c276a7f538009730d8
iriam-2016 <15041338@itdurango.edu.mx>
2019-10-14 22:36:10 -0500

    actualizaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,8 @@ const routes: Routes = [
   { path: 'graficas', loadChildren: './graficas/graficas.module#GraficasPageModule' },
   { path: 'ejercicios', loadChildren: './ejercicios/ejercicios.module#EjerciciosPageModule' },
   { path: 'bitacora', loadChildren: './bitacora/bitacora.module#BitacoraPageModule' },
-  { path: 'add-medicamento', loadChildren: './add-medicamento/add-medicamento.module#AddMedicamentoPageModule' },
+  { path: 'add-medicamento', loadChildren: './add-medicamento/add-medicamento.module#AddMedicamentoPageModule' },  { path: 'receta', loadChildren: './receta/receta.module#RecetaPageModule' },
+
 ];
 
 @NgModule({
