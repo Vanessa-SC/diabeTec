5bca079549f001993f35a99aafc8d328e7cc8674
Vanessa <vanne.jgs@gmail.com>
2019-11-25 19:59:10 -0600

    graficas

--- src/app/graficas/graficas.page.ts
+++ src/app/graficas/graficas.page.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Chart } from 'chart.js';
 
 
@@ -9,100 +6,47 @@ import { Chart } from 'chart.js';
 
 
 @Component({
-  selector: 'app-graficas',
-  templateUrl: './graficas.page.html',
-  styleUrls: ['./graficas.page.scss'],
+	selector: 'app-graficas',
+	templateUrl: './graficas.page.html',
+	styleUrls: ['./graficas.page.scss'],
 })
 export class GraficasPage implements OnInit {
- 
-  
-	private intervalUpdate: any = null;
-	
-	public chart: any = null;
-	
-	constructor(private http: HttpClient) {
-		
-  }
-  
-	ngOnInit(): void {
-		this.chart = new Chart('realtime', {
-			type: 'line',
+
+	@ViewChild("lineCanvas", { static: true }) lineCanvas: ElementRef;
+
+	private lineChart: Chart;
+	constructor() { }
+
+	ngOnInit() {
+		this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+			type: "line",
 			data: {
-				labels: [],
+				labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"],
 				datasets: [
-				  {
-					label: 'Data',
-					fill: false,
-					data: [],
-					backgroundColor: '#168ede',
-					borderColor: '#168ede'
-				  }
+					{
+						label: "Glucosa",
+						fill: false,
+						lineTension: 1,
+						backgroundColor: "rgba(75,192,192,0.4)",
+						borderColor: "rgba(75,192,192,1)",
+						borderCapStyle: "butt",
+						borderDash: [],
+						borderDashOffset: 0.0,
+						borderJoinStyle: "miter",
+						pointBorderColor: "rgba(75,192,192,1)",
+						pointBackgroundColor: "#fff",
+						pointBorderWidth: 1,
+						pointHoverRadius: 5,
+						pointHoverBackgroundColor: "rgba(75,192,192,1)",
+						pointHoverBorderColor: "rgba(220,220,220,1)",
+						pointHoverBorderWidth: 2,
+						pointRadius: 1,
+						pointHitRadius: 10,
+						data: [65, 59, 80, 81, 56, 55, 40],
+						spanGaps: false
+					}
 				]
-			  },
-			  options: {
-				tooltips: {
-					enabled: false
-				},
-				legend: {
-					display: true,
-					position: 'bottom',
-					labels: {
-						fontColor: 'white'
-					}
-				},
-				scales: {
-				  yAxes: [{
-					  ticks: {
-						  fontColor: "white"
-					  }
-				  }],
-				  xAxes: [{
-					ticks: {
-						fontColor: "white",
-						beginAtZero: true
-					}
-				  }]
-				}
-			  }
-		});
-		
-		this.showData();
-		
-		/*this.intervalUpdate = setInterval(function(){
-			this.showData();
-		}.bind(this), 500);*/
-	}
-	
-  ngOnDestroy(): void {
-		clearInterval(this.intervalUpdate);
-	}
-	
-	
-	private showData(): void {
-		this.getFromAPI().subscribe(response => {
-			if(response.error === false) {
-				let chartTime: any = new Date();
-				chartTime = chartTime.getHours() + ':' + ((chartTime.getMinutes() < 10) ? '0' + chartTime.getMinutes() : chartTime.getMinutes()) + ':' + ((chartTime.getSeconds() < 10) ? '0' + chartTime.getSeconds() : chartTime.getSeconds());
-				if(this.chart.data.labels.length > 15) {
-						this.chart.data.labels.shift();
-						this.chart.data.datasets[0].data.shift();
-				}
-				this.chart.data.labels.push(chartTime);
-				this.chart.data.datasets[0].data.push(response.data);
-				this.chart.update();
-			} else {
-				console.error("ERROR: The response had an error, retrying");
 			}
-		}, error => {
-			console.error("ERROR: Unexpected response");
 		});
 	}
-  
-	private getFromAPI(): Observable<any>{
-	  return this.http.get(
-		'http://127.0.0.1:8000/mostrarG/1',
-		{ responseType: 'json' }
-	  );
-  }
-  
 }
